2f3ca055f13be24d2a9b038879914906f7ce02b2
Otto Cedeno <otto@ottostacos.com>
2019-11-07 03:30:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,7 +5,7 @@ test_num = [2, 25, 12, 7]
 def square_array(array)
   # your code here
   square_numbers = Array.new
-  array.each {|n| square_numbers << n**2}
+  array.each { |n| square_numbers << n**2 }
   return square_numbers
 end
 
