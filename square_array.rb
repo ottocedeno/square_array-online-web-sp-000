cd5ed7ef11177ace25062d4047dcc44dce72295f
Otto Cedeno <otto@ottostacos.com>
2019-11-07 03:22:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,6 +5,9 @@ test_num = [2, 25, 12, 7]
 def square_array(array)
   # your code here
   square_numbers = Array.new
+  array.each do | n |
+    square_numbers.unshift(n**2)
+  end
   binding.pry
 end
 
